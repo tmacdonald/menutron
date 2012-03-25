c5df0c25b4315dbaa99cc3ca0e1d6f6edbc0c8ed
Tim Macdonald <macdonald.tim@gmail.com>
2012-03-24 22:33:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Menutron::Application.config.secret_token = '379b2fbcb8ef6c885db1ade6fb837d692a2c489e89d344398f809516621a3c95eae69b19ae08a520a368a96fcb444d812f57989a008dac6bcc91d10f995e6ec6'
