1edf82377a0652375579436c702d6f054cbd7fbd
Tim Macdonald <macdonald.tim@gmail.com>
2012-03-26 21:21:46 -0400

    Added slug to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,14 @@
 class Recipe < ActiveRecord::Base
+  before_save :create_slug
+
+  validates :name, :presence => true
+
+  def create_slug
+    self.slug = self.name.parameterize
+  end
+
+  def to_param
+    "#{id}-#{slug}"
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120327011448) do
+ActiveRecord::Schema.define(:version => 20120327011814) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(:version => 20120327011448) do
     t.integer  "servings"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "slug"
   end
 
 end
